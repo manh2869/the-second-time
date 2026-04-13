98f2f9663512f566bafbea4dc2680f9f646f739e
manh2869 <mank159bmt@gmail.com>
2026-04-13 15:16:03 +0700

    lai la tao day

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+void nextPermutation(vector<int> &nums)
+{
+}
+int main()
+{ //      2 1 3                    1      3      2        1    2    3
+    vector<int> a = {};
+    nextPermutation(a);
+    cout << "i am here too long" << endl;
+    for (int i=0;i<50000 ;i++)        cout << i << endl;
+    return 0;
+}
