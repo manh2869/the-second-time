1b8d67ca619aa33d687ead75339164081ac43595
manh2869 <mank159bmt@gmail.com>
2026-04-13 15:40:54 +0700

    nua ne

--- main.cpp
+++ main.cpp
@@ -4,12 +4,15 @@
 using namespace std;
 void nextPermutation(vector<int> &nums)
 {
+    for (int i = 0; i < 10; i++)
+        cout << "vai loz luon" << endl;
 }
 int main()
 { //      2 1 3                    1      3      2        1    2    3
     vector<int> a = {};
     nextPermutation(a);
     cout << "i am here too long" << endl;
-    for (int i=0;i<50000 ;i++)        cout << i << endl;
+    for (int i = 0; i < 50000; i++)
+        cout << i << endl;
     return 0;
 }
