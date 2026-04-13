ee794edbaf047db80936169dc50c5b93532843a7
manh2869 <mank159bmt@gmail.com>
2026-04-13 13:20:40 +0700

    Implement nextPermutation function in main.cpp
    Added nextPermutation function and included necessary headers.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
-int main()
+#include <vector>
+#include <algorithm>
+using namespace std;
+void nextPermutation(vector<int> &nums)
 {
-    std::cout << "ngu" << std::endl;
+   
+}
+int main()
+{ //      2 1 3                    1      3      2        1    2    3
+    vector<int> a = {};
+    nextPermutation(a);
+    for (int x : a)
+        cout << x << endl;
+    system("pause");
     return 0;
 }
