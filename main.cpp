d6cbbef5c7a1ddbd6aaf680040adb8e258cc3b23
manh2869 <mank159bmt@gmail.com>
2026-04-13 13:38:19 +0700

    Implement nextPermutation and add main example

--- main.cpp
+++ main.cpp
@@ -1,17 +1,37 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
+
 void nextPermutation(vector<int> &nums)
 {
-   
+    int n = static_cast<int>(nums.size());
+    int i = n - 2;
+
+    while (i >= 0 && nums[i] >= nums[i + 1])
+        --i;
+
+    if (i >= 0)
+    {
+        int j = n - 1;
+        while (nums[j] <= nums[i])
+            --j;
+        swap(nums[i], nums[j]);
+    }
+
+    reverse(nums.begin() + i + 1, nums.end());
 }
+
 int main()
-{ //      2 1 3                    1      3      2        1    2    3
-    vector<int> a = {};
+{
+    vector<int> a = {2, 1, 3};
+
     nextPermutation(a);
+
     for (int x : a)
-        cout << x << endl;
-    system("pause");
+        cout << x << ' ';
+    cout << '\n';
+
     return 0;
 }
